3e35b2048240224005694f78c4807d5ef03f3cb3
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-02-07 09:05:42 +0100

    add sequence function

--- slides/index.fsx
+++ slides/index.fsx
@@ -602,13 +602,27 @@ let ``exercise 3.3`` = List.map parseSymbol ["+"; "/"; "12"; "uups"]
 (*** include-value: ``exercise 3.3`` ***)
 (**
 
----
+--- 
+
+### Helper function "sequence"
+
+if all elements are Some values, return Some of those values
+otherwise if there's at least one None, return None *)
+
+let rec sequence (optionals: list<option<'a>>) : option<list<'a>> =
+    match optionals with
+    | [] -> 
+        Some []
+    | None :: _ ->
+        None
+    | Some h :: t ->
+        sequence t |> Option.map (fun t -> h :: t)
 
 
-
+(** ---
 
 ### Exercise 3.4
-Implement `parseSymbols`. Useful functions: `List.map`, `List.forAll`, `Option.isSome`, `Option.get` as well as `splitBy` and `parseSymbol` 
+Implement `parseSymbols`. Useful functions: `List.map`, `sequence` as well as `splitBy` and `parseSymbol` 
 
 #### --------------- Your code goes below --------------- *)
 let parseSymbols (expression: string) : Option<list<Symbol>> =
